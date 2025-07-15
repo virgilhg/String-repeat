2a9a8d453239fbdd321e15124ae9ab7f9f9cc04e
Virgil_HD <120189340+virgilhg@users.noreply.github.com>
2025-07-15 07:39:37 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Solution is incomplete. Challenge was time restrited, with more time, I'd account for faster run time and use a built in method(i.e repeat)
+function repeatStr (n, s) {
+  let str = ""
+  for(let i=0; i=n; i++){
+     str += s
+  }
+  return str;
+}
